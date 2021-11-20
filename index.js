97a0e50b80fb6cc7b6c9bd1b67f119ec79ca3e96
CAMIRO <juancamilosc@hotmail.com>
2021-11-19 19:46:30 -0500

    adding PORT

--- index.js
+++ index.js
@@ -4,3 +4,8 @@ const app =  http.createServer((request, response)=> {
     response.writeHead(200, { 'Content-type':'text/plain' })
     response.end('hello world')
 })
+
+const PORT = 3001
+
+app.listen(PORT)
+console.log(`server running on port ${PORT}`);
